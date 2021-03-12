2b91f5822dbce0409b5e67efb20e12e2eaceda46
gxx <1152862327@qq.com>
2021-03-13 07:58:18 +0800

    添加login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">123</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 15%;
+  height: 10%;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Login = () => import('../components/Login.vue')
+
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
